4b8c0ab40d1e0e64f6c46e3d2dca56da0a70c661
Jon Goh <jonryangoh@gmail.com>
2020-11-11 00:11:09 +0000

    full swing classifier

--- classifyTest/classify2bare.py
+++ classifyTest/classify2bare.py
@@ -29,18 +29,11 @@ import paho.mqtt.client as mqtt
 import numpy as np
 ######JON
 
-RAD_TO_DEG = 57.29578
-M_PI = 3.14159265358979323846
-G_GAIN = 0.070          # [deg/s/LSB]  If you change the dps for gyro, you need to update this value accordingly
-AA =  0.40              # Complementary filter constant
-MAG_LPF_FACTOR = 0.4    # Low pass filter constant magnetometer
-ACC_LPF_FACTOR = 0.4    # Low pass filter constant for accelerometer
-ACC_MEDIANTABLESIZE = 9         # Median filter table size for accelerometer. Higher = smoother but a longer delay
-MAG_MEDIANTABLESIZE = 9         # Median filter table size for magnetometer. Higher = smoother but a longer delay
 
 ######JON
-
 i=0
+ctr=0
+noMotion = True
 downSwing = False
 backswing = False
 
@@ -53,7 +46,6 @@ List4 = []
 List5 = []
 List6 = []
 List7 = []
-
 ######JON
 
 
@@ -90,31 +82,34 @@ while i<500:
     if(i>=25):
         if(ACCx<restingPlace+350 and ACCx>restingPlace-350):   # variable threshold value, good for different stances
             print("No Motion Detected")
+            noMotion = True
+            ctr+=1
+            if(ctr>=4):
+                backSwing = False
+                downSwing = False
         else:
             print("Motion Detected: " + str(myACC))
             if(myACC < restingPlace-500):
                 print("BACK SWING DETECTED!")
                 backSwing = True
+                noMotion = False
+                ctr=0
             elif(myACC > restingPlace+1000):
                 print("DOWN SWING DETECTED!")
                 downSwing = True
+                noMotion = False
+                ctr=0
+
+        if(downSwing == True and backSwing == True):
+            print("COMPLETE SWING!!!!!!!!!!!!!")
+            downSwing = False
+            backSwing = False
 
     #slow program down a bit, makes the output more readable
     time.sleep(0.03)
 
 ######JON
 
-    List.append(round(AccXangle, 3))
-    List1.append(round(AccYangle, 3))
-    List2.append(round(gyroXangle, 3))
-    List3.append(round(gyroYangle, 3))
-    List4.append(round(gyroZangle, 3))
-    List5.append(round(ACCx, 3))
-    List6.append(round(ACCy, 3))
-    List7.append(round(ACCz, 3))
-
     i+=1
 
-print("X ACC:: ")
-print(List5)
 ######JON
