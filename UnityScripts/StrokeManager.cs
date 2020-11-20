b2fb5e8c05746469309ca827a28c173eee13162c
jikaufman <jacobkaufman4@gmail.com>
2020-11-20 15:06:25 -0800

    Basic stroke using MQTT

---
+++ UnityScripts/StrokeManager.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using uPLibrary.Networking.M2Mqtt.Utility;
+using uPLibrary.Networking.M2Mqtt.Exceptions;
+using System;
+
+public class StrokeManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindPlayerBall();
+        MqttSubscriberInit();
+    }
+    
+    Rigidbody playerBallRB;
+    private bool doWhack = false;
+    private double whackVelocity = 0.0;
+     
+    private MqttClient client;
+
+    private void MqttSubscriberInit()
+    {
+        // create client instance 
+        client = new MqttClient("mqtt.eclipse.org");
+
+        // register to message received 
+        client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+        string clientId = Guid.NewGuid().ToString();
+        client.Connect(clientId);
+
+        // subscribe to the topic "ece180da_team5" with QoS 2
+        client.Subscribe(new string[] { "ece180da_team5" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+    }
+
+    private void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+    {
+        Debug.Log("Received: " + System.Text.Encoding.UTF8.GetString(e.Message));
+        whackVelocity = Convert.ToDouble(System.Text.Encoding.UTF8.GetString(e.Message));
+        doWhack = true;
+    }
+
+    private void FindPlayerBall()
+    {
+        GameObject go = GameObject.FindGameObjectWithTag("PlayerBall");
+
+        if (go == null)
+        {
+            Debug.LogError("Couldn't find the ball.");
+        }
+
+        playerBallRB = go.GetComponent<Rigidbody>();
+        if (playerBallRB == null)
+        {
+            Debug.LogError("Player ball has no rigid body.");
+        }
+    }
+
+    // Update is called once per frame -- use this for inputs
+    private void Update()
+    {
+
+    }
+
+    // FixedUpdate runs on every tick of the physics engine -- use this for manipulation
+    void FixedUpdate()
+    {
+        if (playerBallRB == null)
+        {
+            return;
+        }
+        if (doWhack)
+        {
+            doWhack = false;
+
+            Debug.Log("Whacking the ball");
+
+            Vector3 forceVec = new Vector3(0, 0, (float)whackVelocity);
+
+            playerBallRB.AddForce(forceVec, ForceMode.Impulse);
+            //whackVelocity = 0.0;
+        }
+    }
+}
---
+++ UnityScripts/VoiceRecognizer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
+public class VoiceRecognizer : MonoBehaviour
+{
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
+    public Text voicedText = null;
+
+    void Start()
+    {
+
+        actions.Add("good job", GoodJob);
+        actions.Add("good game", GoodGame);
+        actions.Add("let's go", LetsGo);
+        actions.Add("yes", Yes);
+        actions.Add("no", No);
+        actions.Add("nice putt", NicePutt);
+
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+        keywordRecognizer.Start();
+        Debug.Log("listening!");
+    }
+
+    private void RecognizedSpeech(PhraseRecognizedEventArgs speech)
+    {
+        Debug.Log(speech.text);
+        actions[speech.text].Invoke();
+    }
+
+    private void GoodJob()
+    {
+        voicedText.text = "you said: Good Job!";
+    }
+
+    private void GoodGame()
+    {
+        voicedText.text = "you said: Good Game!";
+    }
+
+    private void LetsGo()
+    {
+        voicedText.text = "you said: Let's go!";
+    }
+
+    private void Yes()
+    {
+        voicedText.text = "you said: Yes!";
+    }
+
+    private void No()
+    {
+        voicedText.text = "you said: No!";
+    }
+
+    private void NicePutt()
+    {
+        voicedText.text = "you said: Nice Putt!";
+    }
+
+}
