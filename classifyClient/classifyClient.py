139b7fe4ad1fa831de983525175054c382df1a3b
Jonathan Goh <42307585+jonathanrgoh@users.noreply.github.com>
2020-12-03 11:40:24 -0800

    Update classifyClient.py

--- classifyClient/classifyClient.py
+++ classifyClient/classifyClient.py
@@ -1,8 +1,36 @@
 import time
 import importTest
 
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flags, rc):
+    print("Connection returned result: "+str(rc))
+    client.subscribe("ece180d_team5", qos=1)
+
+def on_disconnect(client, userdata, rc):
+    if rc != 0:
+        print('Unexpected Disconnect')
+    else:
+        print('Expected Disconnect')
+
+def on_message(client, userdata, message):
+    print('Received message: "' + str(message.payload) + '" on topic "' + message.topic + '" with QoS ' + str(message.qos))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+client.on_message = on_message
+
+client.connect_async('mqtt.eclipse.org')
+
+client.loop_start()
+
+
 while True:
     print('jonjon')
-    time.sleep(3)
+    client.subscribe("ece180d_Team5", qos=1)
+    message = client.message.payload
     callFile()
-  
+    
+client.loop_stop()
+client.disconnect()  
