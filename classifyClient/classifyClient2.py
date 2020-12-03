e15aec0f4ee478871358ef88a54483556e5b4c7f
Jonathan Goh <42307585+jonathanrgoh@users.noreply.github.com>
2020-12-03 13:57:40 -0800

    Update classifyClient2.py

--- classifyClient/classifyClient2.py
+++ classifyClient/classifyClient2.py
@@ -7,11 +7,108 @@ import os
 import paho.mqtt.client as mqtt
 import numpy as np
 
-import importTest
-import classifyMqtt2
 
-message1="haha"
+################### CLASSIFIER ###################
+def callClassifier():    
+    i=0
+    ctr=0
+    completeSwingI=0
+    noMotion = True
+    downSwing = False
+    backswing = False
+    completeSwing = False
+    
+    x=list()
+    powerList = []
+    List =[]
+    
+    IMU.detectIMU()     #Detect if BerryIMU is connected.
+    if(IMU.BerryIMUversion == 99):
+        print(" No BerryIMU found... exiting ")
+        sys.exit()
+    IMU.initIMU()       #Initialise the accelerometer, gyroscope and compass
 
+
+    while i<150:
+        #Read the accelerometer,gyroscope and magnetometer values
+        ACCx = IMU.readACCx()
+        ACCy = IMU.readACCy()
+        ACCz = IMU.readACCz()
+        GYRx = IMU.readGYRx()
+        GYRy = IMU.readGYRy()
+        GYRz = IMU.readGYRz()
+        MAGx = IMU.readMAGx()
+        MAGy = IMU.readMAGy()
+        MAGz = IMU.readMAGz()
+
+
+    ######JON
+        if(i==25):
+            restingPlace = myACC
+            print("RESTING VALUE: " + str(restingPlace))
+
+        myACC = IMU.readACCx()
+
+        if(i>=25):
+            if(completeSwing==True):
+                if(completeSwingI+7 == i):
+                    print(powerList)
+                    print("SWING POWER: " + str(max(powerList)))
+
+                    client.publish('ece180da/test', str(max(powerList)), qos=1)
+
+                    powerList.clear()
+                    backSwing = False
+                    downSwing = False
+                    completeSwing = False
+
+            if(ACCx<restingPlace+350 and ACCx>restingPlace-350):   # variable threshold value, good for different stances
+                print("No Motion Detected")
+                noMotion = True
+                ctr+=1
+                if(ctr>=4):
+                    backSwing = False
+                    downSwing = False
+                    completeSwing = False
+
+            else:
+                print("Motion Detected: " + str(myACC))
+                if(myACC < restingPlace-500):
+                    print("BACK SWING DETECTED!")
+                    backSwing = True
+                    downSwing = False
+                    noMotion = False
+                    ctr=0
+                elif(myACC > restingPlace+450):
+                    print("DOWN SWING DETECTED!")
+                    downSwing = True
+                    noMotion = False
+                    ctr=0
+
+            if(completeSwing == True):
+                downSwing =False
+                backSwing =False
+
+            if(downSwing == True and backSwing == True):
+                print("     COMPLETE SWING!!!!!!!!!!!!!")
+                completeSwing = True
+                downSwing = False
+                backSwing = False
+                powerList = List[i-5:]
+                completeSwingI=i
+
+            if(completeSwing == True):
+                powerList.append(round(ACCx,3))
+
+        #slow program down a bit, makes the output more readable
+        time.sleep(0.03)
+
+        List.append(round(ACCx,3))
+        i+=1
+################### CLASSIFIER ###################
+
+
+################### MQTT ###################
 def on_connect(client, userdata, flags, rc):
     print("Connection returned result: "+str(rc))
     client.subscribe("ece180d/test", qos=1)
@@ -24,7 +121,8 @@ def on_disconnect(client, userdata, rc):
 
 def on_message(client, userdata, message):
     print('Received message: "' + str(message.payload) + '" on topic "' + message.topic + '" with QoS ' + str(message.qos))
-    classifyMqtt2.callClassifier()
+    callClassifier()
+    #classifyMqtt2.callClassifier()
     #importTest.callFile()
     #message1 = str(message.payload)
 
@@ -40,9 +138,8 @@ client.loop_start()
 
 while True:
     pass
-    #print('jonjon')
-    #print(message1)
-    #importTest.callFile()
     
 client.loop_stop()
 client.disconnect()  
+################### MQTT ###################
+
