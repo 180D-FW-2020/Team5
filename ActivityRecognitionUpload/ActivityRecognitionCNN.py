bc0fc6128c142fdcef0a0078f94d339a355a4982
Greyson Beck <gbeck17@brophybroncos.org>
2020-12-04 15:12:39 -0800

    Updated Architecture and Data Formatting

--- ActivityRecognitionUpload/ActivityRecognitionCNN.py
+++ ActivityRecognitionUpload/ActivityRecognitionCNN.py
@@ -1,21 +1,50 @@
 import tensorflow as tf
 import imageio
-import keras
+from tensorflow import keras
 from keras.models import Sequential
 from keras import layers, Model, optimizers, callbacks
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dense, Dropout, Flatten, Input
+from keras.layers import Conv1D, MaxPooling1D, BatchNormalization, Embedding
+from keras.preprocessing import sequence
+from sklearn.model_selection import train_test_split
 import numpy as np
 import cv2 
 import pandas as pd
 import os
+from keras.models import load_model
+import h5py
+#from formDataset.py import df_to_dataset, demo
+
+# Load Data
+data = pd.read_csv(r'C:\Users\stink\180DA\ActivityRecognition\TrainingData.csv')
+data['Label'] = pd.Categorical(data['Label'])
+data['Label'] = data.Label.cat.codes
+#train, val = train_test_split(data,test_size=.2)
+xtrain = data.loc[:,'1x':'25conf']
+ytrain = data.loc[:,'Label']
+
+#Convert to numpy array 
+trainarraydata = xtrain.to_numpy()
+trainarraylabels = ytrain.to_numpy()
+trainarraydatanew = np.reshape(trainarraydata,(5069,75,1))
+#print(trainarraydatanew[0])
 
 # Define Model Architecture
 model = Sequential()
-model.add(Conv1D(16, kernel_size=(3,3), activation='relu',input_shape=(18,2))
+model.add(Conv1D(16, 5, activation='relu',input_shape=(75,1)))
+#model.add(MaxPooling1D(5))
 model.add(BatchNormalization())
 model.add(Dropout(.25))
 model.add(Flatten())
-model.add(Dense(1,activation='sigmoid',name='output'))
+model.add(Dense(1,activation='sigmoid'))
 
+print(model.summary())
+
+opt = optimizers.Adam(learning_rate=.0001)
+model.compile(loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              optimizer=opt,
+              metrics=['acc'])
+
+model.fit(trainarraydatanew, trainarraylabels,validation_split=.2, shuffle=True, batch_size=32, 
+    epochs=50)
+model.save('ActRecognition.h5')
--- ActivityRecognitionUpload/FormDataset.py
+++ ActivityRecognitionUpload/FormDataset.py
@@ -2,30 +2,71 @@ import pandas as pd
 import json
 import csv
 import os
+import sklearn
+from sklearn.model_selection import train_test_split
+import numpy as np
 
-data1 = os.listdir('C:/users/stink/180da/ReadyStance')
-csvFile = 'C:/users/stink/180da/activityrecognition/Dataset2.csv'
+data1 = os.listdir('C:/users/stink/180da/OtherStance')
+csvFile = 'C:/users/stink/180da/activityrecognition/OtherStanceDataset.csv'
 dataset = [[] for i in range(len(data1))]
 datasetFinal = []
 count = 0
+
 for filename in data1:
-    framePath = ('C:/users/stink/180da/ReadyStance/' + filename)
+    framePath = ('C:/users/stink/180da/OtherStance/' + filename)
     with open(framePath,'r') as f:
         frame = json.load(f)
     dataset[count] = frame
     count+=1
 #for k in range(len(dataset)):
     #dataset[k] = dataset[k]['people']
+
 for i in range(len(dataset)):
-    keypoints = dataset[i]['people'][0]['pose_keypoints_2d'] # extract keypoints for a given frame
-    datasetFinal.append(keypoints)
+    if dataset[i]['people']:
+        keypoints = dataset[i]['people'][0]['pose_keypoints_2d'] # extract keypoints for a given frame
+        datasetFinal.append(keypoints)
 file = open(csvFile,'w+', newline='')
 with file: 
     write = csv.writer(file)
     write.writerows(datasetFinal)
 
+dataset = pd.read_csv(r'C:\Users\stink\180DA\ActivityRecognition\TrainingData.csv')
+train, val = train_test_split(dataset, test_size=.2)
+xtrain = train.loc[:,'1x':'25conf']
+ytrain = train.loc[:,'Label']
+xval = val.loc[:,'1x':'25conf']
+yval = val.loc[:,'Label']
 
+trainarraydata = xtrain.to_numpy()
+trainarraylabels = ytrain.to_numpy()
+valarraydata = xval.to_numpy()
+valarraylabels = yval.to_numpy()
+print(trainarraydata.shape)
+print(trainarraylabels.shape)
+
+
+def df_to_dataset(dataframe, shuffle=True, batch_size=32):
+  dataframe = dataset.copy()
+  labels = dataframe.pop('target')
+  #ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
+  if shuffle:
+    ds = ds.shuffle(buffer_size=len(dataframe))
+  ds = ds.batch(batch_size)
+  return ds
+
+#train_ds = df_to_dataset(train)
+#val_ds = df_to_dataset(val, shuffle=False)
+
+#example_batch = next(iter(train_ds))[0]
+def demo(feature_column):
+  example_batch = next(iter(train_ds))[0]
+  feature_layer = layers.DenseFeatures(feature_column)
+  print(feature_layer(example_batch).numpy())
+
+
+
+
+#dataset.set_option('display.max_rows',none,'display.max_columns',none)
+#print(dataset.loc[5000,'1x':'25conf'])
 #dataset.write('C:/users/stink/180da/ActivityRecognition/dataset.txt')
 #filedf.to_csv('C:/users/stink/180da/ActivityRecognition/Dataset.csv')
-
-
