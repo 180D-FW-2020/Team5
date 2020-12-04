067e8504094dca8438ef254c3b575bff73e52e27
Jonathan Goh <42307585+jonathanrgoh@users.noreply.github.com>
2020-12-04 15:41:56 -0800

    Update classifyClient2.py

--- classifyClient/classifyClient2.py
+++ classifyClient/classifyClient2.py
@@ -156,7 +156,7 @@ def on_disconnect(client, userdata, rc):
 
 def on_message(client, userdata, message):
     print('Received message: "' + str(message.payload) + '" on topic "' + message.topic + '" with QoS ' + str(message.qos))
-    print(message.payload)
+    print(message.payload.decode("UTF-8"))
     if(message.payload=="startClassifier"):
         callClassifier()
     if(message.payload=="startButtons"):
