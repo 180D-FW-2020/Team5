ea00cc1f1ff3469052103c2d004046bf8c87acd2
Jonathan Goh <42307585+jonathanrgoh@users.noreply.github.com>
2021-02-02 14:39:32 -0800

    Update mainNoPose.py

--- main/mainNoPose.py
+++ main/mainNoPose.py
@@ -242,9 +242,6 @@ while True:
     if (client.message == playerName + ",startButtons"):
         callButtons()
         client.message=""
-    if (client.message == playerName + ",startPose"):
-        callPose()
-        client.message=""
     pass
     
 client.loop_stop()
